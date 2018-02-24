ade7b837e1ee4cce842b8e2181632fd831d112cd
QiuDong <dong.qiu@madv360.com>
2018-02-24 12:17:51 +0800

    switchSVN.sh

---
+++ MADVCameraFramework/switchSVN.sh
@@ -0,0 +1,61 @@
+
+cd MADVCamera/XMLManager
+svn switch svn://172.10.30.102/madV1/branches/app/ios/source/branch/V1.8.9.17379_02.12_bug/Madv360_v1/XMLManager@$1
+cd ..
+
+cd MADVCamera/ThirdParty
+svn switch svn://172.10.30.102/madV1/branches/app/ios/source/branch/V1.8.9.17379_02.12_bug/Madv360_v1/ThirdParty@$1
+cd ..
+
+cd MADVCamera/CameraManager
+svn switch svn://172.10.30.102/madV1/branches/app/ios/source/branch/V1.8.9.17379_02.12_bug/Madv360_v1/CameraManager@$1
+cd ..
+
+cd MADVCamera/Renderer
+svn switch svn://172.10.30.102/madV1/branches/app/ios/source/branch/V1.8.9.17379_02.12_bug/Madv360_v1/Renderer@$1
+cd ..
+
+cd MADVCamera/PhotosManager
+svn switch svn://172.10.30.102/madV1/branches/app/ios/source/branch/V1.8.9.17379_02.12_bug/Madv360_v1/PhotosManager@$1
+cd ..
+
+cd MADVCamera/DBHelper
+svn switch svn://172.10.30.102/madV1/branches/app/ios/source/branch/V1.8.9.17379_02.12_bug/Madv360_v1/DBHelper@$1
+cd ..
+
+cd MADVCamera/DataModel
+svn switch svn://172.10.30.102/madV1/branches/app/ios/source/branch/V1.8.9.17379_02.12_bug/Madv360_v1/DataModel@$1
+cd ..
+
+cd MADVCamera/Views
+svn switch svn://172.10.30.102/madV1/branches/app/ios/source/branch/V1.8.9.17379_02.12_bug/Madv360_v1/Views@$1
+cd ..
+
+cd MADVCamera/Utils
+svn switch svn://172.10.30.102/madV1/branches/app/ios/source/branch/V1.8.9.17379_02.12_bug/Utils@$1
+cd ..
+
+cd MADVCamera/FFMpegExt
+svn switch svn://172.10.30.102/madV1/branches/app/ios/source/FFmpeg/FFMpegExt@$1
+cd ..
+
+cd MADVCamera/kxmovie
+svn switch svn://172.10.30.102/madV1/branches/app/ios/source/kxmovie@$1
+cd ..
+
+cd MADVCamera/CycordKit
+svn switch svn://172.10.30.102/madV1/branches/app/ios/source/MADVPano/trunk/CycordKit/@$1
+cd ..
+
+cd MADVCamera/MVGLView.h
+svn switch svn://172.10.30.102/madV1/branches/app/ios/source/MADVPano/trunk/platforms/MVGLView.h@$1
+cd ..
+
+cd MADVCamera/MVGLView.mm
+svn switch svn://172.10.30.102/madV1/branches/app/ios/source/MADVPano/trunk/platforms/MVGLView.mm@$1
+cd ..
+
+cd MADVCamera/libs/FFmpeg
+svn switch svn://172.10.30.102/madV1/branches/app/ios/source/FFmpeg/FFmpeglibs@$1
+cd ..
+
